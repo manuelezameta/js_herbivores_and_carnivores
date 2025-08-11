48502159579e4732357a84b922fd726f67678d38
Manuel Lezameta <manuelezameta@gmail.com>
2025-08-11 17:12:07 -0500

    Addressed feedback

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -34,11 +34,7 @@ class Carnivore extends Animal {
       prey.health -= 50;
 
       if (prey.health <= 0) {
-        const index = Animal.alive.indexOf(prey);
-
-        if (index > -1) {
-          Animal.alive.splice(index, 1);
-        }
+        Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
       }
     }
   }
